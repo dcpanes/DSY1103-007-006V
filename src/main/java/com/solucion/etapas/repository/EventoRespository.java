111330ad7a196d2c29a4864c78cca653abeccf57
Dusty Tessini <dcpanes@gmail.com>
2026-04-07 21:13:58 -0400

    repository completo

--- src/main/java/com/solucion/etapas/repository/EventoRespository.java
+++ src/main/java/com/solucion/etapas/repository/EventoRespository.java
@@ -8,10 +8,34 @@ import java.util.List;
 
 @Repository
 public class EventoRespository {
-
     private List<Evento> eventos = new ArrayList<>();
 
     public List<Evento> findAll(){
         return eventos;
+    }
+
+    public Evento create(Evento evento){
+        eventos.add(evento);
+        return evento;
+    }
+
+    public boolean delete(String id){
+        return eventos.removeIf(evento -> evento.getId().equals(id));
+    }
+
+    public Evento findById(String id){
+        return eventos.stream()
+                .filter(evento -> evento.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Evento update(String id, Evento nuevoEvento){
+        Evento existeEvento = findById(id);
+        if(existeEvento != null){
+            existeEvento.setTipoEvento(nuevoEvento.getTipoEvento());
+            return existeEvento;
+        }
+        return null;
     }
 }
